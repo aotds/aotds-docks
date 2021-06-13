ec63a80d4223422ef9cbbb6d36edfe16efe29c51
Yanick Champoux <yanick@babyl.ca>
2021-06-13 12:18:29 -0400

    make the css imports part of the svelte template

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,20 @@
 <App />
 
-<script>
-  import App from '../components/App.svelte';
-</script>
+<svelte:head>
+  {#each stylesheets as href (href)}
+    <link rel="stylesheet" {href} />
+  {/each}
+</svelte:head>
 
+<script>
+  import { base } from "$app/paths";
+  import App from "../components/App.svelte";
+
+  const stylesheets = [
+    "/bulma/bulma.css",
+    "/fonts/faktos.css",
+    "/fonts/dosis/dosis.css",
+    "/global.css",
+  ].map((ss) => base + ss);
+
+</script>
