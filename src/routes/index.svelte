b51b27e43b7635aa688c9123e2a2d5c39dbfc138
Yanick Champoux <yanick@babyl.ca>
2022-02-28 15:25:34 -0500

    put a minimal working App.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,9 @@
 <App />
 
-<svelte:head>
-  {#each stylesheets as href (href)}
-    <link rel="stylesheet" {href} />
-  {/each}
-</svelte:head>
-
 <script>
   import '../../static/fonts/faktos.css';
   import '../../static/fonts/dosis/dosis.css';
   import '../../static/global.css';
 
   import App from "$lib/components/App.svelte";
-
 </script>
