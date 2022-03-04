9596ae99226193bb580c39d4d91ea5fb45e76d94
Yanick Champoux <yanick@babyl.ca>
2022-03-04 16:17:57 -0500

    cargo

--- src/lib/components/ShipEdit/Structure/index.svelte
+++ src/lib/components/ShipEdit/Structure/index.svelte
@@ -1,15 +1,18 @@
 <Section label="structure">
     <Hull {...hull}/>
     <Screens {...screens} />
+    <Cargo {...cargo} />
 </Section>
 
 <script>
   import Section from "$lib/components/Section/index.svelte";
   import Hull from './Hull.svelte';
   import Screens from './Screens.svelte';
+  import Cargo from './Cargo.svelte';
 
   export let hull = {};
   export let screens = {};
+  export let cargo = {};
 
 
 </script>
--- src/lib/shipDux/structure/index.js
+++ src/lib/shipDux/structure/index.js
@@ -2,8 +2,9 @@ import { Updux } from 'updux';
 
 import hull from './hull.js';
 import screens from './screens.js';
+import cargo from './cargo.js';
 
 const dux = new Updux({
-    subduxes: { hull, screens }
+    subduxes: { hull, screens, cargo }
 });
 export default dux;
