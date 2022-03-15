d9b186a5aacee9e46962813d3bc7f64474d4b7bc
Yanick Champoux <yanick@babyl.ca>
2022-03-15 10:59:33 -0400

    main layout

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,9 +1,12 @@
 <Ribbon />
 <Header />
 
+<ShipEdit />
+
 <script>
   import Ribbon from "./Ribbon.svelte";
   import Header from "./Header.svelte";
+  import ShipEdit from './ShipEdit/index.svelte';
 </script>
 
 <style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,15 @@
 <App />
 
 <script>
+  import { setContext } from "svelte";
+
+
   import "../../static/fonts/faktos.css";
   import "../../static/fonts/dosis/dosis.css";
   import "../../static/global.css";
+  import shipStore from "$lib/store/ship.js";
 
   import App from "$lib/components/App.svelte";
+
+  setContext("ship", shipStore());
 </script>
