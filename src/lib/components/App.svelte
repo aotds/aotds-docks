c8e32a9a142f0fe641e19d6c929de05bc6d28249
Yanick Champoux <yanick@babyl.ca>
2022-03-20 17:48:45 -0400

    add about component

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,13 +1,25 @@
 <Ribbon />
 <Header />
 
+
 <ShipEdit />
 
 <script>
+import {
+    Modal, Card, Nav
+} from 'svelte-chota';
+
   import Ribbon from "./Ribbon.svelte";
   import Header from "./Header.svelte";
   import ShipEdit from './ShipEdit/index.svelte';
+import About from './About.svelte';
+
 </script>
 
 <style>
+.nav {
+        width: var(--main-width);
+        margin-left: auto;
+        margin-right: auto;
+}
 </style>
