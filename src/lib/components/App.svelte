69d8e4dd84c7183ff450b0f28475db3906b87525
Yanick Champoux <yanick@babyl.ca>
2022-03-20 17:55:51 -0400

    prettier

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,25 +1,21 @@
 <Ribbon />
 <Header />
 
-
 <ShipEdit />
 
 <script>
-import {
-    Modal, Card, Nav
-} from 'svelte-chota';
+    import { Modal, Card, Nav } from "svelte-chota";
 
-  import Ribbon from "./Ribbon.svelte";
-  import Header from "./Header.svelte";
-  import ShipEdit from './ShipEdit/index.svelte';
-import About from './About.svelte';
-
+    import Ribbon from "./Ribbon.svelte";
+    import Header from "./Header.svelte";
+    import ShipEdit from "./ShipEdit/index.svelte";
+    import About from "./About.svelte";
 </script>
 
 <style>
-.nav {
+    .nav {
         width: var(--main-width);
         margin-left: auto;
         margin-right: auto;
-}
+    }
 </style>
