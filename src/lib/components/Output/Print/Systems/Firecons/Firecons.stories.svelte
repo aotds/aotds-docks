0d3c053705a0c6fa19874a412f55877384c12382
Yanick Champoux <yanick@babyl.ca>
2022-03-26 13:55:12 -0400

    print firecons

---
+++ src/lib/components/Output/Print/Systems/Firecons/Firecons.stories.svelte
@@ -0,0 +1,17 @@
+<Meta title="Output/Print/Systems/Firecons" component={Firecons} argTypes={{
+    firecons: { defaultValue: 2, type: 'number' }
+}} />
+
+<Story name="Primary" args={{}} />
+
+<Template let:args>
+  <div style="width: 50em">
+    <Firecons {...args}/>
+  </div>
+</Template>
+
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+
+  import Firecons from './index.svelte';
+</script>
