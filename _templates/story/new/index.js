745a72f83fe13c86487978c8e61e0302e5105579
Yanick Champoux <yanick@babyl.ca>
2022-03-26 14:04:24 -0400

    print screens

--- _templates/story/new/index.js
+++ _templates/story/new/index.js
@@ -4,8 +4,10 @@ module.exports = {
   params: ({ args }) => {
 
     const storypath = args.path.replace( '.svelte', '.stories.svelte' );
-    const component = path.basename(args.path).replace('.svelte','');
 
-    return { storypath, component}
+    const component = path.basename(args.path.replace('/index.svelte','')).replace('.svelte','');
+
+    const to = path.join(path.dirname(args.path), component + '.stories.svelte' );
+    return { storypath, component, to}
   }
 }
