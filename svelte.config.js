5d540b76750f5cf640b3fbc175f459e9b4bb1228
Yanick Champoux <yanick@babyl.ca>
2022-04-02 21:03:47 -0400

    with the path

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,22 @@ import analyze from "rollup-plugin-analyzer";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  kit: {
-//        outDir: './docs',
-    // hydrate the <div id="svelte"> element in src/app.html
-        adapter: adapter({
-			fallback: 'index.html'
-        }),
-    vite: {
-      build: {
-        rollupOptions: {
-          plugins: [analyze()],
-          //            external: ['updux','@yanick/updeep']
+    kit: {
+        //        outDir: './docs',
+        // hydrate the <div id="svelte"> element in src/app.html
+        paths: {
+            base: "/aotds-docks",
         },
-      },
+        adapter: adapter({
+            fallback: "index.html",
+        }),
+        vite: {
+            build: {
+                rollupOptions: {
+                    plugins: [analyze()],
+                    //            external: ['updux','@yanick/updeep']
+                },
+            },
+        },
     },
-  },
 };
