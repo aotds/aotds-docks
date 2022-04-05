9e7922066cff4a6f3d9aeaffa18a86cef029a2d4
Yanick Champoux <yanick@babyl.ca>
2022-04-05 19:36:36 -0400

    add base to config

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,16 @@ import analyze from "rollup-plugin-analyzer";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    adapter: adapter(),
-    vite: {
-      build: {
-        rollupOptions: {
-          plugins: [analyze()],
-          //            external: ['updux','@yanick/updeep']
+    kit: {
+        adapter: adapter(),
+        paths: { base: "/aotds-docks" },
+        vite: {
+            build: {
+                rollupOptions: {
+                    plugins: [analyze()],
+                    //            external: ['updux','@yanick/updeep']
+                },
+            },
         },
-      },
     },
-  },
 };
