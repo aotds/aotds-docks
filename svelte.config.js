4c4adf6fb41dc00f90bfd2bc5494c71360afcece
Yanick Champoux <yanick@babyl.ca>
2022-04-05 20:00:18 -0400

    new page version

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import adapter from "@sveltejs/adapter-static";
 import analyze from "rollup-plugin-analyzer";
 
+const dev = process.env.NODE_ENV === 'development';
+
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
     kit: {
-        adapter: adapter(),
-        paths: { base: "/aotds-docks" },
+        adapter: adapter({ fallback: 'index.html' }),
+        paths: { base: dev? '' : "/aotds-docks" },
         vite: {
             build: {
                 rollupOptions: {
