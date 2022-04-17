255b29601234e5c42180f51b70cb4dcd47abb550
Yanick Champoux <yanick@babyl.ca>
2022-04-17 12:27:33 -0400

    remove a console.log

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,54 +1,51 @@
 <svelte:head>
-    <meta name="viewport" content="width=960" />
+  <meta name="viewport" content="width=960" />
 </svelte:head>
 
 <main>
+  <Ribbon />
+  <Header on:changeTab={({ detail }) => (activeTab = detail)} />
 
-<Ribbon />
-<Header on:changeTab={({ detail }) => activeTab = detail}/>
-
-<div class:hide={activeTab !== 'editor'}>
+  <div class:hide={activeTab !== "editor"}>
     <ShipEdit />
-</div>
-<div class:hide={activeTab !== 'json'}>
+  </div>
+  <div class:hide={activeTab !== "json"}>
     <JsonOutput />
-</div>
+  </div>
 
-{#if activeTab === 'print'}
-    <PrintOutput ship={$state}/>
-{/if}
-
+  {#if activeTab === "print"}
+    <PrintOutput ship={$state} />
+  {/if}
 </main>
 
 <script>
-import { getContext } from 'svelte';
-    import { Modal, Card, Nav } from "svelte-chota";
+  import { getContext } from "svelte";
+  import { Modal, Card, Nav } from "svelte-chota";
 
-    import Ribbon from "./Ribbon.svelte";
-    import Header from "./Header.svelte";
-    import ShipEdit from "./ShipEdit/index.svelte";
-    import About from "./About.svelte";
-    import JsonOutput from './Output/Json.svelte';
-    import PrintOutput from './Output/Print/index.svelte';
+  import Ribbon from "./Ribbon.svelte";
+  import Header from "./Header.svelte";
+  import ShipEdit from "./ShipEdit/index.svelte";
+  import About from "./About.svelte";
+  import JsonOutput from "./Output/Json.svelte";
+  import PrintOutput from "./Output/Print/index.svelte";
 
-    let activeTab = 'editor';
-    $: console.log(activeTab);
+  let activeTab = "editor";
 
-    const {state} = getContext('ship');
+  const { state } = getContext("ship");
 </script>
 
 <style>
-    .nav {
-        width: var(--main-width);
-        margin-left: auto;
-        margin-right: auto;
-    }
-    .hide {
-        display: none;
-    }
-    main {
-        width: var(--main-width);
-        margin-right: auto;
-        margin-left: auto;
-    }
+  .nav {
+    width: var(--main-width);
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .hide {
+    display: none;
+  }
+  main {
+    width: var(--main-width);
+    margin-right: auto;
+    margin-left: auto;
+  }
 </style>
