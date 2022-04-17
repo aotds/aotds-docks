82331d1f26cf4355dcb465c3a3391d0ab2679a56
Yanick Champoux <yanick@babyl.ca>
2022-04-17 17:53:31 -0400

    chore(release): 2.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,20 +2,29 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
-## [2.2.0](///compare/v2.1.0...v2.2.0) (2022-04-11)
+## 2.3.0 (2022-04-17)
 
 
 ### Features
 
-* allow to edit the print layout 381d497
+* add 'reset ship' button 
+* add changelog to app 
+* add version to About component 
 
-## [2.1.0](///compare/v2.0.0...v2.1.0) (2022-04-07)
+## 2.2.0 (2022-04-11)
 
 
 ### Features
 
-* add localStorage memory bf7206a
-* can move main system comps in the print output b425f4a
+* allow to edit the print layout 
+
+## 2.1.0 (2022-04-07)
 
 
-* add standard-version 2fd047f
+### Features
+
+* add localStorage memory 
+* can move main system comps in the print output 
+
+
+* add standard-version 
--- src/lib/components/Changelog.svelte
+++ src/lib/components/Changelog.svelte
@@ -1,38 +1,21 @@
 <h1 id="changelog">Changelog</h1>
-<p>
-  All notable changes to this project will be documented in this file. See <a
-    href="https://github.com/conventional-changelog/standard-version"
-    >standard-version</a
-  > for commit guidelines.
-</p>
-<h2 id="230comparev220v23020220417">
-  <a href="///compare/v2.2.0…v2.3.0">2.3.0</a> (2022-04-17)
-</h2>
+<p>All notable changes to this project will be documented in this file. See <a href="https://github.com/conventional-changelog/standard-version">standard-version</a> for commit guidelines.</p>
+<h2 id="23020220417">2.3.0 (2022-04-17)</h2>
 <h3 id="features">Features</h3>
 <ul>
-  <li>
-    <p>
-      add 'reset ship' button [GH<a
-        href="undefined/undefined/undefined/issues/15">#15</a
-      >] 49fc8d5
-    </p>
-  </li>
-  <li><p>add version to About component 919f286, closes #6</p></li>
-  <li><p><strong>release:</strong> 2.3.0 4d10119</p></li>
+<li>add 'reset ship' button </li>
+<li>add changelog to app </li>
+<li>add version to About component </li>
 </ul>
-<h2 id="220comparev210v22020220411">
-  <a href="///compare/v2.1.0…v2.2.0">2.2.0</a> (2022-04-11)
-</h2>
+<h2 id="22020220411">2.2.0 (2022-04-11)</h2>
 <h3 id="features-1">Features</h3>
 <ul>
-  <li>allow to edit the print layout 381d497</li>
+<li>allow to edit the print layout </li>
 </ul>
-<h2 id="210comparev200v21020220407">
-  <a href="///compare/v2.0.0…v2.1.0">2.1.0</a> (2022-04-07)
-</h2>
+<h2 id="21020220407">2.1.0 (2022-04-07)</h2>
 <h3 id="features-2">Features</h3>
 <ul>
-  <li><p>add localStorage memory bf7206a</p></li>
-  <li><p>can move main system comps in the print output b425f4a</p></li>
-  <li><p>add standard-version 2fd047f</p></li>
+<li><p>add localStorage memory </p></li>
+<li><p>can move main system comps in the print output </p></li>
+<li><p>add standard-version </p></li>
 </ul>
