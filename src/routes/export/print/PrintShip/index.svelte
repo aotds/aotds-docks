b0f670fe4ae487af1f8f583b765cd093cca3ad40
Yanick Champoux <yanick@babyl.ca>
2023-04-26 09:53:22 -0400

    major updates

--- src/routes/export/print/PrintShip/index.svelte
+++ src/routes/export/print/PrintShip/index.svelte
@@ -1,17 +1,17 @@
 <div>Printing this page will only prints the ship sheet.</div>
 
 <div class="print-output">
-  Identification {identification} />
+  <Identification {...identification} />
 
   <div class="section-2" />
 
-  MainSystems {propulsion} />
+  <MainSystems {...propulsion} />
 </div>
 
 <script>
-  //import Identification from "./Identification.svelte";
-  //  import MainSystems from "./MainSystems/index.svelte";
-  //  import Hull from "./Hull.svelte";
+  import Identification from "./Identification.svelte";
+  import MainSystems from "./MainSystems/index.svelte";
+  import Hull from "./Hull.svelte";
 
   export let identification = {};
   export let propulsion = {};
@@ -25,13 +25,13 @@
     padding: 1em;
     margin: 0px auto;
   }
-  /* @media print { */
-  /*   :global(body > *) { */
-  /*     visibility: hidden; */
-  /*   } */
+  @media print {
+    :global(body > *) {
+      visibility: hidden;
+    }
 
-  /*   .print-output { */
-  /*     visibility: visible; */
-  /*   } */
-  /* } */
+    .print-output {
+      visibility: visible;
+    }
+  }
 </style>
