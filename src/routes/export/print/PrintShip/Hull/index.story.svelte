cab1036498c5dfa1df61abe758f3b10eb9fe5ec1
Yanick Champoux <yanick@babyl.ca>
2023-04-26 16:05:17 -0400

    story for print hull

---
+++ src/routes/export/print/PrintShip/Hull/index.story.svelte
@@ -0,0 +1,21 @@
+<Hst.Story title="Export/PrintShip/Hull">
+  <Hst.Variant>
+    <Hull {rating} {advanced} {shipMass} />
+  </Hst.Variant>
+
+  <svelte:fragment slot="controls">
+    <Hst.Number bind:value={rating} title="rating" />
+    <Hst.Checkbox bind:value={advanced} title="advanced" />
+    <Hst.Number bind:value={shipMass} title="ship mass" />
+  </svelte:fragment>
+</Hst.Story>
+
+<script>
+  import Hull from "./index.svelte";
+
+  export let Hst;
+
+  let rating = 12;
+  let advanced = true;
+  let shipMass = 10;
+</script>
