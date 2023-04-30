288fc0a907844b5a12134c9d34ba3e4f8a3c9307
Yanick Champoux <yanick@babyl.ca>
2023-04-30 12:08:42 -0400

    add print cargo

---
+++ src/routes/export/print/PrintShip/Cargo.svelte
@@ -0,0 +1,33 @@
+{#if space}
+  <div class="grid">
+    <div class="s2">cargo</div>
+    <div class="s10">
+      {#each Array.from({ length: space }) as _, i (i)}
+        <div class="cell" />
+      {/each}
+    </div>
+  </div>
+{/if}
+
+<script>
+  export let space;
+</script>
+
+<style>
+  .cell {
+    display: inline-block;
+    margin-right: 0.5em;
+    margin-top: 0.5em;
+    width: 1.5em;
+    height: 1.5em;
+    border: 1px solid black;
+  }
+  div.s2 {
+    margin-top: 0.5em;
+    text-align: right;
+  }
+  div.m9 {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
