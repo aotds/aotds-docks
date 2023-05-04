5739652d77ddcdef2f2c3f90e245379aee4c6f4e
Yanick Champoux <yanick@babyl.ca>
2023-05-04 18:42:07 -0400

    right link for repo

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 // vite.config.js
 import { sveltekit } from "@sveltejs/kit/vite";
+import packageJson from "./package.json";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()],
-    publicDir: "./static",
-    ssr: {},
-    optimizeDeps: {},
+  plugins: [sveltekit()],
+  publicDir: "./static",
+  ssr: {},
+  optimizeDeps: {},
+  define: {
+    "import.meta.env.PACKAGE_VERSION": JSON.stringify(packageJson.version),
+    "import.meta.env.HOMEPAGE": JSON.stringify(packageJson.homepage),
+  },
 };
 
 export default config;
