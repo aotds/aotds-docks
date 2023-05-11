2ef72888f5ed6ee1f8b8dede09bcd616c29095c0
Yanick Champoux <yanick@babyl.ca>
2023-05-11 10:11:55 -0400

    add version in the about page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 // vite.config.js
 import { sveltekit } from "@sveltejs/kit/vite";
 import packageJson from "./package.json";
+import getVersion from "git-repo-version";
+import git from "git-describe";
 
 /** @type {import('vite').UserConfig} */
 const config = {
@@ -9,7 +11,7 @@ const config = {
     ssr: {},
     optimizeDeps: {},
     define: {
-        "import.meta.env.PACKAGE_VERSION": JSON.stringify(packageJson.version),
+        "import.meta.env.PACKAGE_VERSION": git.gitDescribeSync().semverString,
         "import.meta.env.HOMEPAGE": JSON.stringify(packageJson.homepage),
         "import.meta.env.BUGS": JSON.stringify(packageJson.bugs.url),
     },
